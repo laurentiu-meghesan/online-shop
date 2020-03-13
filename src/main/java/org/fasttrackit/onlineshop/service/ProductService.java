cb2dca06e4a77b6d9412cb0eb47dc804d37f0e04
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2020-03-13 19:23:57 +0200

    added getProducts method in ProductService

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -3,14 +3,15 @@ package org.fasttrackit.onlineshop.service;
 import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistance.ProductRepository;
-import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
+import org.fasttrackit.onlineshop.transfer.product.GetProductsRequest;
+import org.fasttrackit.onlineshop.transfer.product.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -56,7 +57,22 @@ public class ProductService {
                 ("Product " + id + "not found."));
     }
 
-    public Product updateProduct(long id, SaveProductRequest request){
+    public Page<Product> getProducts(GetProductsRequest request, Pageable pageable) {
+
+        LOGGER.info("Searching products: {}", request);
+
+        if (request != null) {
+            if (request.getPartialName() != null && request.getMinQuantity() != null){
+                return productRepository.findByNameContainingAndQuantityGreaterThanEqual(request.getPartialName(),
+                        request.getMinQuantity(), pageable);
+            } else if (request.getPartialName() != null){
+                return productRepository.findByNameContaining(request.getPartialName(),pageable);
+            }
+        }
+        return productRepository.findAll(pageable);
+    }
+
+    public Product updateProduct(long id, SaveProductRequest request) {
         LOGGER.info("Updating product {}: {}", id, request);
         Product product = getProduct(id);
 
@@ -65,9 +81,10 @@ public class ProductService {
         return productRepository.save(product);
     }
 
-    public void deleteProduct(long id){
+    public void deleteProduct(long id) {
         LOGGER.info("Deleting product {}", id);
         productRepository.deleteById(id);
     }
+
 
 }
