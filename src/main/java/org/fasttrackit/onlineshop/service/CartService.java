afe52c1712292a422e08abb0efeaa5cb965f9e0c
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2020-03-25 19:23:58 +0200

    fixed lazy loading exceptions in cart service

--- src/main/java/org/fasttrackit/onlineshop/service/CartService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/CartService.java
@@ -3,14 +3,20 @@ package org.fasttrackit.onlineshop.service;
 import org.fasttrackit.onlineshop.domain.Cart;
 import org.fasttrackit.onlineshop.domain.Customer;
 import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistance.CartRepository;
 import org.fasttrackit.onlineshop.transfer.cart.AddProductsToCartRequest;
+import org.fasttrackit.onlineshop.transfer.cart.CartResponse;
+import org.fasttrackit.onlineshop.transfer.cart.ProductInCartResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 @Service
 public class CartService {
@@ -26,6 +32,7 @@ public class CartService {
         this.cartRepository = cartRepository;
         this.customerService = customerService;
         this.productService = productService;
+
     }
 
     @Transactional
@@ -46,5 +53,36 @@ public class CartService {
         }
 
         cartRepository.save(cart);
+    }
+
+//    returning DTO to avoid Lazy Loading exceptions
+    @Transactional
+    public CartResponse getCart(long customerId){
+        LOGGER.info("Retrieving cart items for customer {}", customerId);
+
+        Cart cart = cartRepository.findById(customerId).orElseThrow(() -> new ResourceNotFoundException
+                ("Cart " + customerId + " does not exist."));
+
+        CartResponse cartResponse = new CartResponse();
+        cartResponse.setId(cart.getId());
+
+        Set<ProductInCartResponse> productDtos = new HashSet<>();
+
+        Iterator<Product> productIterator = cart.getProducts().iterator();
+
+        while (productIterator.hasNext()){
+            Product nextProduct = productIterator.next();
+
+            ProductInCartResponse productDto = new ProductInCartResponse();
+            productDto.setId(nextProduct.getId());
+            productDto.setName(nextProduct.getName());
+            productDto.setPrice(nextProduct.getPrice());
+
+            productDtos.add(productDto);
+        }
+
+        cartResponse.setProducts(productDtos);
+
+        return cartResponse;
     }
 }
